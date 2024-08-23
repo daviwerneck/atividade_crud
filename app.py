6d1f3d481cd5348a3a655d0d71b86cdf66751a3f
daviwerneck <davijwerneck@gmail.com>
2024-08-23 08:41:16 -0300

    newsgit add .

--- app.py
+++ app.py
@@ -1,6 +1,17 @@
 from flask import Flask, render_template, request, flash
 app = Flask(__name__)
+from database import db
+from flask_migrate import Migrate
+from models import Usuario
 app.config['SECRET_KEY'] = 'cd6ec8a03ee6252de83e921bd4be5e016819ed51221599fea40e5cbfd110ecce'
+
+# drive://usuario:senha@servidor/banco_de_dados
+conexao = "mysql+pymysql://alunos:cefetmg@127.0.0.1/flaskg2"
+app.config['SQLALCHEMY_DATABASE_URI'] = conexao
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db.init_app(app)
+migrate = Migrate(app, db)
 
 @app.route('/')
 def index():
@@ -23,6 +34,12 @@ def dados():
     flash('Dados enviados!!')
     dados = request.form
     return render_template('dados.html', dados=dados)
+
+@app.route('/usuario')
+def usuario():
+    u = Usuario.query.all()
+    return render_template('usuario_lista.html', dados = u)
+
 
 if __name__ == '__main__':
     app.run()
